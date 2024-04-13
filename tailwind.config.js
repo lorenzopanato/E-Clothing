9282eec39e91af350a078bf41985bd6b97124b72
Lorenzo <lorenzohpanato@gmail.com>
2024-04-13 11:17:26 -0300

    feat: add initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Jost", "sans-serif"],
+      serif: ["DM Sans", "serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#121212",
+        secondary: "#E0E0E0",
+        tertiary: "#616161",
+        quaternary: "#757575",
+        editGray: "#f6f6f6",
+      },
+    },
+  },
+  plugins: [],
+};
